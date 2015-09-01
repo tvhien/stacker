026c3143665684d561588a55256dad05e724911e
ebh <alex.dess@ansarada.com>
2015-09-01 11:41:57 +1000

    Added logging for AWS config

--- lib/stacker/stack.rb
+++ lib/stacker/stack.rb
@@ -55,7 +55,7 @@ JSON
 
     def client
       @client ||= region.client.stacks[name]
-      AWS.config(:logger => Logger.new($stdout))
+      # AWS.config(:logger => Logger.new($stdout))
       # Stacker.logger.debug 'Stack config max_retries: Blah'
     end
 
