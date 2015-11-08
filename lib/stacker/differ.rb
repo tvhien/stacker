b94a726bec7f2b0a367e0e188693a4c25198da1a
ebh <alex.dess@ansarada.com>
2015-11-08 14:25:51 +1100

    Revert "Improved logging"
    This reverts commit 7456fd223514e23103f5a8e8656bb38227187cbb.

--- lib/stacker/differ.rb
+++ lib/stacker/differ.rb
@@ -7,10 +7,8 @@ module Stacker
     module_function
 
     def diff one, two, *args
-      Stacker.logger.info "Starting diff"
       down = args.include? :down
 
-      Stacker.logger.info "Starting Diffy"
       diff = Diffy::Diff.new(
         (down ? one : two) + "\n",
         (down ? two : one) + "\n",
@@ -18,7 +16,6 @@ module Stacker
         include_diff_info: true
       ).to_s(*args.select { |arg| arg == :color })
 
-      Stacker.logger.info "Starting strip"
       diff.gsub(/^(\x1B.+)?(\-{3}|\+{3}).+\n/, '').strip
     end
 
