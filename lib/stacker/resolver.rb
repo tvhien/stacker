8382934c972d3cb70d0756eace77e62b0e36f65c
Daniel Tan <daniel.tan@ansarada.com>
2020-08-27 13:57:05 +1000

    fix up stacks

--- lib/stacker/resolver.rb
+++ lib/stacker/resolver.rb
@@ -15,25 +15,30 @@ module Stacker
       }
     end
 
+    def resolveRegion region, stack, output
+      puts "Grabbing output from region #{region} for stack: #{stack} and output #{output}"
+      cfnClient = Aws::CloudFormation::Client.new(region: region)
+      resp = cfnClient.describe_stacks({
+        stack_name: value.fetch('Stack')
+      })
+      if resp.stacks.length == 0
+        raise "No stacks found called #{stack}"
+      elsif resp.stacks.length > 1
+        raise "More than 1 stacks found called #{stack}. Found #{resp.stacks.length}"
+      end
+      for output in resp.stacks[0].outputs do
+        if output.output_key == output
+          return output.output_value
+        end
+      end
+      raise "No output found for key #{output}"
+    end
+
     def resolved
     @resolved ||= Hash[parameters.map do |name, value|
       if value.is_a? Hash
-        if value.key?("Region")
-          puts "Grabbing output from region #{value.fetch('Region')} for stack: #{value.fetch('Stack')}"
-          cfnClient = Aws::CloudFormation::Client.new(region: value.fetch('Region'))
-          resp = cfnClient.describe_stacks({
-            stack_name: value.fetch('Stack')
-          })
-          puts "Retrieved stacks: #{resp.stacks.length}"
-          puts "Outputs in stack: #{resp.stacks[0].outputs.length}"
-          for output in resp.stacks[0].outputs do
-            puts "Looking at output_key #{output.output_key}"
-            if output.output_key == value.fetch('Output')
-              value = output.output_value
-              break
-            end
-          end
-          puts "retrieved value: #{value}"
+        if value.key?('Region')
+          value = resolveRegion value.fetch('Region') value.fetch('Stack') value.fetch('Output')
         else
           stack = region.GetStack value.fetch('Stack')
           value = stack.outputs.fetch value.fetch('Output')
